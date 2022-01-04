97b5853a75427a7536152d4154e5e4ed7d2a8a20
TECH SCOUT <techsc0utOP@gmail.com>
2022-01-04 12:52:06 +0530

    Update 0-1_Knapsack.java

--- 0-1_Knapsack.java
+++ 0-1_Knapsack.java
@@ -1,22 +1,17 @@
 class Solution { 
     static int knapSack(int W, int wt[], int val[], int n){ 
-        int i, w; 
-        int K[][]=new int[n+1][W+1]; 
-        K[0][0]=0;
-        for(i=1;i<=n;i++){ 
-            for(w=0;w<=W;w++){ 
-                //if weight of current item is more than Knapsack capacity W
-                //then this item cannot be included in the optimal solution.
-                if(wt[i-1]<=w) {
-                    K[i][w]=Math.max(val[i-1]+K[i-1][w-wt[i-1]],K[i-1][w]);    
+        int dp[][]=new int[n+1][W+1];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=W;j++){
+                if(wt[i-1]<=j){
+                    dp[i][j]=Math.max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
                 }
-                //else updating K[i][w] as K[i-1][w].
-                else{
-                    K[i][w] = K[i-1][w];     
-                }
-             } 
-        } 
-        //returning the result.
-        return K[n][W]; 
+            }
+        }
+        return dp[n][W];
     } 
 }
+
+

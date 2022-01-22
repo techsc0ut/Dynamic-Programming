e7748b9c856075472ad9b5baeac8b79acd389cf6
TECH SCOUT <techsc0utOP@gmail.com>
2022-01-22 21:54:06 +0530

    Update Maximum_Sum_Increasing_Subsequence.java

--- Maximum_Sum_Increasing_Subsequence.java
+++ Maximum_Sum_Increasing_Subsequence.java
@@ -1,19 +1,25 @@
 class Solution{
-	public int maxSumIS(int arr[],int n){  
-	    int res=arr[0];
+	public int maxSumIS(int arr[], int n){  
+	    if(n==1){
+	        return arr[0];
+	    }
+	    int max=-1;
 	    int dp[]=new int[n];
-	    dp[0]=arr[0];
+	    for(int i=0;i<n;i++){
+	        dp[i]=arr[i];
+	        if(max<dp[i]){
+	            max=dp[i];
+	        }
+	    }
 	    for(int i=1;i<n;i++){
-	        int max=0;
 	        for(int j=0;j<i;j++){
-	            if(arr[i]>arr[j]){
-	                if(max<dp[j]){
-	                    max=dp[j];
-	                }
+	            if(arr[j]<arr[i]){
+	                dp[i]=Math.max(dp[i],dp[j]+arr[i]);
 	            }
 	        }
-	        dp[i]=max+arr[i];
-	        res=Math.max(res,dp[i]);
-	    }return res;
+	        if(max<dp[i]){
+	            max=dp[i];
+	        }
+	    }return max;
 	}  
 }

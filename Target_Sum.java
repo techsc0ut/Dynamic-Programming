d5aeaeec9f7eb4096b15ed5899ace7c6dba1fbf6
TECH SCOUT <techsc0utOP@gmail.com>
2022-03-22 01:11:36 +0530

    Explanation Added

--- Target_Sum.java
+++ Target_Sum.java
@@ -1,6 +1,3 @@
-/*
-  Thanks to aditya verma for his approach :)
-*/
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         if(nums.length==1){
@@ -15,6 +12,12 @@ class Solution {
         if(target>curSum || (curSum-target)%2!=0){
             return 0;            
         }
+	/* Problem reduced to s1-s2=target =>(Eq-1) [because if we seperate +elements and -elements, then we can say
+		s1=(+a,+b,+c) and s2=(-c,-d,-e) and now problem reduced to s1-s2=target]
+		but again we know that s1+s2=arraySum =>(Eq-2)
+			=> adding equation 1 and 2
+			=> s1=(target+arraySum)/2
+		so finally problem gets reduced to finding count of subsets with given sum i.e s1 in our case*/
         int s1=(target+curSum)/2;
         return countSubsetSum(nums,nums.length,s1);
     }

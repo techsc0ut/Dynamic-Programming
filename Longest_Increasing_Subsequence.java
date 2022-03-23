1c8e05666da9197f4e43258a61016ffefd34ab30
TECH SCOUT <techsc0utOP@gmail.com>
2022-03-24 01:40:03 +0530

    Update Longest_Increasing_Subsequence.java

--- Longest_Increasing_Subsequence.java
+++ Longest_Increasing_Subsequence.java
@@ -24,5 +24,27 @@ class Solution {
     }
 }
 
+/* Top Down DP TABLE */
+class Solution {
+    public int longestCommonSubsequence(String s1, String s2) {
+        int dp[][]=new int[s1.length()+1][s2.length()+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }return dp[s1.length()][s2.length()];
+    }
+}
+
 
 

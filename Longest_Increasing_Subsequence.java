7c99fc26a2b06f5c4f962d89c09461ca278d510e
TECH SCOUT <techsc0utOP@gmail.com>
2022-03-24 01:04:01 +0530

    memoized version

--- Longest_Increasing_Subsequence.java
+++ Longest_Increasing_Subsequence.java
@@ -1,18 +1,28 @@
+/* Memoized Solution */
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int dp[]=new int[nums.length];
-        dp[0]=1;int res=1;
-        for(int i=1;i<dp.length;i++){
-            int max=0;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    if(max<dp[j]){
-                        max=dp[j];
-                    }
-                }
-            }
-            dp[i]=max+1;
-            res=Math.max(dp[i],res);
-        }return res;
+    static int dp[][];
+    public int longestCommonSubsequence(String s1, String s2) {
+        dp=new int[s1.length()+1][s2.length()+1];
+        for(int x[]:dp){
+            Arrays.fill(x,-1);
+        }
+        return LCS(s1,s2,s1.length(),s2.length());
+    }
+    public int LCS(String s1, String s2, int n, int m){
+        if(n==0 || m==0){
+            return dp[n][m]=0;
+        }
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(s1.charAt(n-1)==s2.charAt(m-1)){
+            return dp[n][m]=1+LCS(s1,s2,n-1,m-1);
+        }
+        else{
+            return dp[n][m]=Math.max(LCS(s1,s2,n-1,m),LCS(s1,s2,n,m-1));
+        }
     }
 }
+
+
+

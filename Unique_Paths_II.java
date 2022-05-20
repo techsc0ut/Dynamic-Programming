588c05e91b52c9ae6da868aca62f9c95ae0b855e
TECH SCOUT <techsc0utOP@gmail.com>
2022-05-20 11:29:10 +0530

    Update Unique_Paths_II.java

--- Unique_Paths_II.java
+++ Unique_Paths_II.java
@@ -1,3 +1,31 @@
+/* DP Solution */
+class Solution {
+    public int uniquePathsWithObstacles(int[][] m) {
+        if(m[0][0]==1) return 0;
+        int dp[][]=new int[m.length][m[0].length];
+        dp[0][0]=1;
+        for(int i=1;i<m.length;i++){
+            if(m[i][0]==0){
+                dp[i][0]=dp[i-1][0];    
+            }
+        }
+        for(int i=1;i<m[0].length;i++){
+            if(m[0][i]==0){
+                dp[0][i]=dp[0][i-1];
+            }
+        }
+        for(int i=1;i<m.length;i++){
+            for(int j=1;j<m[0].length;j++){
+                if(m[i][j]==0){
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                }
+            }
+        }
+        return dp[m.length-1][m[0].length-1];
+    }
+}
+
+/* Recursive Memoized */
 class Solution {
     int dp[][];
     public int uniquePathsWithObstacles(int[][] m) {

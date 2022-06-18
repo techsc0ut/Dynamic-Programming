89c43eee5f3905f7e09ffbcf626abb42e128cb88
TECH SCOUT <techsc0utOP@gmail.com>
2022-06-18 17:07:38 +0530

    Update Subset_Sum_Problem.java

--- Subset_Sum_Problem.java
+++ Subset_Sum_Problem.java
@@ -1,25 +1,13 @@
 class Solution{
-    static Boolean isSubsetSum(int N,int arr[],int sum){
+    static Boolean isSubsetSum(int N, int arr[], int sum){
         boolean dp[][]=new boolean[N+1][sum+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=true;
-                }else if(i==0){
-                    dp[i][j]=false;
-                }else if(j==0){
-                    dp[i][j]=true;
+        dp[0][0]=true;
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
                 }else{
-                    if(dp[i-1][j]==true){                  // Batting Nahi krega , to check kar pichle bande bna payenge pure runs .
-                         dp[i][j]=true; 
-                    }else{                                 // Batting krega to de check kr , remaining run bna payenge pichle bande .
-                        int val=arr[i-1];
-                        if(j>=val){
-                            if(dp[i-1][j-val]==true){
-                                dp[i][j]=true;
-                            }
-                        }
-                    }
+                    dp[i][j]=dp[i-1][j];
                 }
             }
         }return dp[N][sum];

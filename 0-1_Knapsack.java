a37b075539c4913d940a815f2369a4301bdd10cf
TECH SCOUT <techsc0utOP@gmail.com>
2022-06-18 15:18:02 +0530

    Update 0-1_Knapsack.java

--- 0-1_Knapsack.java
+++ 0-1_Knapsack.java
@@ -1,3 +1,30 @@
+/* Recursive + Memoization */
+class Solution{ 
+    static int dp[][];
+    static int knapSack(int W, int wt[], int val[], int N) { 
+        dp=new int[W+1][N+1];
+        for(int x[]:dp){
+            Arrays.fill(x,-1);
+        }
+        return func(W,N,wt,val);
+    } 
+    static int func(int sackWt, int lastItem, int wt[], int val[]){
+       if(lastItem==0 || sackWt==0){
+           return dp[sackWt][lastItem]=0;
+       }
+       if(dp[sackWt][lastItem]!=-1){
+           return dp[sackWt][lastItem];
+       }
+       if(wt[lastItem-1]<=sackWt){
+           return dp[sackWt][lastItem]=Math.max(val[lastItem-1]+func(sackWt-wt[lastItem-1],lastItem-1,wt,val),
+           func(sackWt,lastItem-1,wt,val));
+       }else{
+           return dp[sackWt][lastItem]=func(sackWt,lastItem-1,wt,val);
+       }
+    }
+}
+
+/* Tabulation DP*/
 class Solution { 
     static int knapSack(int W, int wt[], int val[], int n){ 
         int dp[][]=new int[n+1][W+1];

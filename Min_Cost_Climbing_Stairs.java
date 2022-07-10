f2570535a443b7065bdcbd0c6009e2b245c1ab0d
TECH SCOUT <techsc0utOP@gmail.com>
2022-07-10 12:11:33 +0530

    Update Min_Cost_Climbing_Stairs.java

--- Min_Cost_Climbing_Stairs.java
+++ Min_Cost_Climbing_Stairs.java
@@ -1,3 +1,4 @@
+/* Memoized Recursive */
 class Solution {
     int dp[];
     public int minCostClimbingStairs(int[] nums) {
@@ -14,5 +15,20 @@ class Solution {
             return dp[i];
         }
         return dp[i]=nums[i]+Math.min(func(nums,i-1),func(nums,i-2));
+    }
+}
+
+/* DP TABLE */
+class Solution {
+    public int minCostClimbingStairs(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        for(int i=0;i<n;i++){
+            if(i==0 || i==1){
+                dp[i]=nums[i];
+            }else{
+                dp[i]=nums[i]+Math.min(dp[i-1],dp[i-2]);
+            }
+        }return Math.min(dp[n-1],dp[n-2]);
     }
 }
